5b74d5816df230603e4de6fa777b6d9218353d9e
sen <5.22nhz@gmail.com>
2024-11-02 13:44:22 +0900

    Refactor: 테스트 코드 삭제 및 코드 정리

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import signup, login, logout, index, home, check_username, toggle_bgm, test
+from .views import signup, login, logout, index, home, check_username, toggle_bgm
 
 urlpatterns = [
     path('', index, name='index'),
@@ -9,5 +9,4 @@ urlpatterns = [
     path('home/', home, name='home'),
     path('check-username/', check_username, name='check_username'),
     path('toggle-bgm/', toggle_bgm, name='toggle_bgm'),
-    path('test/', test, name='test'),
 ]
