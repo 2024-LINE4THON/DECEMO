4d82afcc16139a4884d0626a61889ba01bfa9377
mzxxzysy <jsyjj21@gmail.com>
2024-11-07 17:13:23 +0900

    질문 상세 페이지2

--- static/js/question_detail.js
+++ static/js/question_detail.js
@@ -1,3 +1,18 @@
+//입력했을 때 저장 버튼 색 바꾸기
+const submit_btn = document.querySelector(".submit");
+const text_box = document.querySelector("textarea");
+
+text_box.addEventListener("input", function () {
+  if (text_box.value) {
+    submit_btn.style.backgroundColor = "#ffd739";
+    submit_btn.disabled = false;
+  } else {
+    submit_btn.style.backgroundColor = ""; // 기본색
+    submit_btn.disabled = true;
+  }
+});
+
+//저장 버튼 누르면 모달창 띄우기
 const modal = document.querySelector(".modal");
 const modalOpen = document.querySelector(".submit-btn button");
 const modalClose = document.querySelector(".close_btn");
